48fb9df748084648211d478cc34cf02dfad10af4
anamackey <59660518+anamackey@users.noreply.github.com>
2020-01-13 21:32:44 -0700

    java

---
+++ index.js
@@ -0,0 +1,36 @@
+let daynames = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+
+
+
+
+]
